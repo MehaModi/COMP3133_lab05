2289650460a33d8215e0a84699c98d20f940c1ba
meham <mehamodi9@gmail.com>
2024-03-10 23:24:59 -0400

    Lab05

---
+++ Readme.md
@@ -0,0 +1,4 @@
+COMP 3133 – Full Stack Development II – Lab05
+
+STUDENT NAME - Meha Modi
+STUDENT ID - 101371431
---
+++ exercise-4/main.ts
@@ -0,0 +1,5 @@
+import { Customer } from './customer';
+
+let customer = new Customer("John", "Doe", 21);
+customer.greeter();
+customer.getAge();
